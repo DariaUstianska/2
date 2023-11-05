ad929c87d654fe57f3f78bd8f29cc8ad880e68c7
Daria Ustianska <dariaustianska@gmail.com>
2023-11-05 18:16:45 +0200

    js

---
+++ main.js
@@ -0,0 +1,5 @@
+let inputFirstNumber = Number (prompt('Введіть перше число'));
+let inputSecondNumber = Number (prompt('Введіть друге число'));
+let inputThirdNumber = Number (prompt('Введіть третє число'));
+let average = (inputFirstNumber + inputSecondNumber + inputThirdNumber) / 3;
+alert(`Середнє значення: ${average}`)
